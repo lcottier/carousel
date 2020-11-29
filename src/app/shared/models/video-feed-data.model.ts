9818a816f7fabc55377da06f24cfebea17cd8c6b
lcottier <lcottier94@googlemail.com>
2020-11-29 13:43:01 +0000

    Add models for feed data

---
+++ src/app/shared/models/video-feed-data.model.ts
@@ -0,0 +1,62 @@
+export class VideoFeedData {
+    feedMetaData: VideoFeedMetaData;
+    sections: Array<VideoFeedSections>
+}
+
+export class VideoFeedMetaData {
+    description: string;
+    id: string;
+    name: string;
+    target: string;
+    title: string;
+}
+
+export class VideoFeedSections {
+    id: string;
+    name: string;
+    pagingData: VideoFeedPagingData;
+    itemData: Array<VideoFeedItems>;
+}
+
+export class VideoFeedPagingData {
+    itemCount: number;
+    pageCount: number;
+    pageIndex: number;
+    pageSize: number;
+    totalCount: number;
+}
+
+export class VideoFeedItems {
+    id: string;
+    publicationData: VideoFeedItemPublicationData;
+    mediaData: VideoFeedItemMediaData;
+    metaData: VideoFeedItemMetaData;
+}
+
+export class VideoFeedItemPublicationData {
+    createdAt: string;
+    releaseFrom: string;
+    releaseTo: string;
+    updatedAt: string;
+    released: boolean;
+}
+
+export class VideoFeedItemMediaData {
+    entryId: string;
+    entryStatus: string;
+    mediaType: string;
+    thumbnailUrl: string;
+}
+
+export class VideoFeedItemMetaData {
+    SysEntryEntitlements: Array<any>;
+    VideoDuration: number;
+    contenttags: Array<string>;
+    corecategories: Array<string>;
+    people: Array<string>;
+    tags: Array<string>;
+    teams: Array<string>;
+    tvshowcategories: Array<string>;
+    title: string;
+    body: any;
+}
