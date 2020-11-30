660dd52111c81c8733fb6e3a4ed3b7f2d294e853
lcottier <lcottier94@googlemail.com>
2020-11-30 20:08:31 +0000

    Make carousel a more generic component. Feed section through input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { VisualisationApiService } from './shared/services/api/visualisations-api/visualisations-api.service';
+import { VideoFeedData, VideoFeedSection } from './shared/models/video-feed-data.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'carousel';
+  sections = new Array<VideoFeedSection>();
+  subscriptions = new Subscription();
+
+  constructor(private visualisationApiService: VisualisationApiService, private changeDetection: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.subscriptions.add(this.visualisationApiService.getVideoFeed().subscribe((feedData: VideoFeedData) => {
+      this.sections = feedData.sections;
+      this.changeDetection.detectChanges();
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
